ef34dbf501b405b726179f5b41ca4f335c70ea71
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-05-02 19:18:17 -0700

    Update d1y-p4r71.sh

--- d1y-p4r71.sh
+++ d1y-p4r71.sh
@@ -14,14 +14,11 @@
 cp -r device/* ./
 rm -rf device
 
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-
 sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
 
 sed -i "s/%R/by 0xACE8/" package/base-files/files/etc/openwrt_release
 
-sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;0n3cl0ud' feeds.conf.default
-sed -i '2 i\src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
-sed -i '3 i\src-git openwrt_helloworld https://github.com/sbwml/openwrt_helloworld' feeds.conf.default
+sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463' feeds.conf.default
+sed -i '2 i\src-git openwrt_helloworld https://github.com/sbwml/openwrt_helloworld' feeds.conf.default
 #sed -i '$a src-git xd https://github.com/shiyu1314/onecloud-package' feeds.conf.default
 
