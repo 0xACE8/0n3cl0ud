91bafcc83ce498cf11071ee2e34a1c65a0c973c2
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-05-04 02:46:03 -0700

    Update d1y-p4r72.sh

--- d1y-p4r72.sh
+++ d1y-p4r72.sh
@@ -20,12 +20,30 @@ sed -i 's/ash/bash/g' package/base-files/files/etc/passwd
 sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/ace8/luci-app-argon-config/po/zh_Hans/argon-config.po
 
 # change some tips
-sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 sed -i 's/nas/services/g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
-#sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+
+# ttyd terminal patch
 sed -i '/interface}/d' feeds/packages/utils/ttyd/files/ttyd.init
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+
+# dnscrypt-proxy2 patch
+sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/timeout:-5/timeout:-120/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/ipv6_servers = false/ipv6_servers = true/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/odoh_servers = false/odoh_servers = true/g' /feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/http3 = false/http3 = true/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/cache_size = 4096/cache_size = 10240/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/127.0.0.1:9050/127.0.0.1:1070/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/# proxy/proxy/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+sed -i 's/# blocked_names_file/blocked_names_file/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+#sed -i 's/127.0.0.53:53/127.0.0.1:5335/g' /etc/dnscrypt-proxy2/dnscrypt-proxy.toml
+#sed -i 's/127.0.0.1:8888/127.0.0.1:1101/g' /etc/dnscrypt-proxy2/dnscrypt-proxy.toml
+#sed -i 's/# http_proxy/http_proxy/g' /etc/dnscrypt-proxy2/dnscrypt-proxy.toml
+wget --no-check-certificate https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md -O feeds/packages/net/dnscrypt-proxy2/files/public-resolvers.md
+wget --no-check-certificate https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md.minisig -O feeds/packages/net/dnscrypt-proxy2/files/public-resolvers.md.minisig
+wget --no-check-certificate https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md -O feeds/packages/net/dnscrypt-proxy2/files/relays.md
+wget --no-check-certificate https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md.minisig -O feeds/packages/net/dnscrypt-proxy2/files/relays.md.minisig
 
 # DHCP
 #mkdir -p package/base-files/files/etc/dnsmasq.d
@@ -39,12 +57,7 @@ echo 'net.core.wmem_max=26214400' >>package/base-files/files/etc/sysctl.d/10-def
 echo 'net.core.wmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
 echo 'net.core.netdev_max_backlog=2048' >>package/base-files/files/etc/sysctl.d/10-default.conf
 
-#5. Change luci list name
-#sed -i 's/5e72e4/1c77ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-#sed -i 's/5e72e4/1c77ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
-#sed -i 's/483d8b/1c77ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-#sed -i 's/483d8b/1c77ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
-
+# argon icon flush new desgin
 rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/favicon.ico
 wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/favicon.ico "https://raw.githubusercontent.com/0xACE8/0n3cl0ud/main/argon/favicon.ico"
 rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
@@ -63,9 +76,6 @@ wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/ar
 
 #rm -rf include/target.mk
 #wget --no-check-certificate -O include/target.mk "https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/include/target.mk"
-
-#rm -rf feeds/ace8/kcptun/Makefile
-#wget --no-check-certificate -O feeds/ace8/kcptun/Makefile "https://raw.githubusercontent.com/0xACE8/kcptun/20230214/Makefile"
 
 # Change to my banner
 sudo rm package/base-files/files/etc/banner
