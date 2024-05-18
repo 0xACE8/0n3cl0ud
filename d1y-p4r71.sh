63d250cd7ce31d7f27ccb8cb6b9c661a0e605b2e
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-05-18 03:11:28 -0700

    Update d1y-p4r71.sh

--- d1y-p4r71.sh
+++ d1y-p4r71.sh
@@ -9,16 +9,13 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# git_sparse_clone main https://github.com/shiyu1314/openwrt-onecloud device
-
-cp -r device/* ./
-rm -rf device
+#git_sparse_clone 6.6 https://github.com/shiyu1314/openwrt-onecloud target
+#cp -r device/* ./
+#rm -rf device
 
 sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
-
 sed -i "s/%R/by 0xACE8/" package/base-files/files/etc/openwrt_release
-
 sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463' feeds.conf.default
-sed -i '2 i\src-git openwrt_helloworld https://github.com/sbwml/openwrt_helloworld' feeds.conf.default
-#sed -i '$a src-git xd https://github.com/shiyu1314/onecloud-package' feeds.conf.default
+sed -i '2 i\src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+
 
